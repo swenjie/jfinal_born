01c418eadd2cea91b5e7f7206bbf3d4b461e2a58
swenjie <shangwenjie1026@126.com>
2019-08-06 12:49:31 +0800

    ReadMe.md

--- com.born.jf/src/main/java/com/born/jf/erp/test/controller/HelloController.java
+++ com.born.jf/src/main/java/com/born/jf/erp/test/controller/HelloController.java
@@ -3,12 +3,16 @@ package com.born.jf.erp.test.controller;
 import com.born.jf.erp.test.bean.TestBean;
 import com.jfinal.core.Controller;
 
-public class HelloController  extends Controller{
+public class HelloController extends Controller {
 	public void index() {
-	       renderText("Hello JFinal World.");
-	    }
-	
+		renderText("Hello JFinal World.");
+	}
+
 	public void getBean() {
-		TestBean testBean=new TestBean();
+		TestBean testBean = new TestBean();
+		testBean.setAge("0");
+		testBean.setName("lb");
+		testBean.setSex("M");
+		renderJson(testBean);
 	}
 }
